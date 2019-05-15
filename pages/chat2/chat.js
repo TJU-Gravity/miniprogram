5ad002ae3f891df372faadeca7e84daa23364d66
Rebecca-yh <1652818@tongji.edu.cn>
2019-05-15 16:03:06 +0800

    chat2

---
+++ pages/chat2/chat.js
@@ -0,0 +1,163 @@
+// pages/chat/chat.js
+
+const app = getApp();
+var inputVal = '';
+var msgList = [];
+var windowWidth = wx.getSystemInfoSync().windowWidth;
+var windowHeight = wx.getSystemInfoSync().windowHeight;
+var keyHeight = 0;
+
+/**
+ * 初始化数据
+ */
+function initData(that) {
+  inputVal = '';
+
+  msgList = [{
+    speaker: 'you',
+    contentType: 'text',
+    content: '你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！'
+  },
+  {
+    speaker: 'me',
+    contentType: 'text',
+    content: '再见你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！你好！'
+  }
+  ]
+  that.setData({
+    msgList,
+    inputVal
+  })
+}
+
+/**
+ * 计算msg总高度
+ */
+// function calScrollHeight(that, keyHeight) {
+//   var query = wx.createSelectorQuery();
+//   query.select('.scrollMsg').boundingClientRect(function(rect) {
+//   }).exec();
+// }
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    meHeadIcon:null,
+    youHeadIcon:"http://pic.9ht.com/up/2016-12/14810057988524092.jpg",
+    scrollHeight: '100vh',
+    inputBottom: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("onload")
+    if (app.globalData.userInfo) {
+
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+         meHeadIcon: app.globalData.userInfo.headshot
+      })
+    } else if (this.data.canIUse) {
+      console.log("delay")
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        initData(this);
+        this.setData({
+          userInfo: res.data.data,
+          meHeadIcon: res.dat.data.headshot,
+          hasUserInfo: true
+        })
+      }
+    } 
+    initData(this);
+    if (app.globalData.userInfo) {
+ 
+    this.setData({
+      meHeadIcon: app.globalData.userInfo.headshot,
+    });
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 获取聚焦
+   */
+  focus: function (e) {
+    keyHeight = e.detail.height;
+    this.setData({
+      scrollHeight: (windowHeight - keyHeight) + 'px'
+    });
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1),
+      inputBottom: keyHeight + 'px'
+    })
+    //计算msg高度
+    // calScrollHeight(this, keyHeight);
+
+  },
+
+  //失去聚焦(软键盘消失)
+  blur: function (e) {
+    this.setData({
+      scrollHeight: '100vh',
+      inputBottom: 0
+    })
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1)
+    })
+
+  },
+
+  /**
+   * 发送点击监听
+   */
+  sendClick: function (e) {
+    msgList.push({
+      speaker: 'me',
+      contentType: 'text',
+      content: e.detail.value
+    })
+    inputVal = '';
+    this.setData({
+      msgList,
+      inputVal
+    });
+
+
+  },
+
+  /**
+   * 退回上一页
+   */
+  toBackClick: function () {
+    wx.navigateBack({})
+  }
+
+})
