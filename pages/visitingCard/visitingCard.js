2d0b32b3dab87ad976d00ec337f159a23ab4b3fe
Rebecca-yh <1652818@tongji.edu.cn>
2019-05-22 16:38:58 +0800

    updateVisitingCard

--- pages/visitingCard/visitingCard.js
+++ pages/visitingCard/visitingCard.js
@@ -1,6 +1,10 @@
 //home.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+var util = require('../../utils/util.js'); // 转换时间插件
+var im = require('../../utils/webim_wx.js'); // 腾讯云 im 包
+var imhandler = require('../../utils/im_handler.js'); // 这个是所有 im 事件的 js
+
 
 Page({
   data: {
@@ -23,6 +27,49 @@ Page({
       url: '../info/info'
       //url: '../post/post'
     })
+  },
+  clickContact(event)
+  {
+
+    var that = this;
+    //如果已经初始化过userSig参数,直接进入一对一私聊，传入朋友的identifier，name，headshhot
+    //否则先访问后台，初始化userSig参数，获取到userSig后，如果没有登陆先登陆再进入一对一私聊
+    if (app.data.im.userSig) {
+      //转至一对一的聊天界面(自己的openid和对方的openid)，聊天
+      wx.navigateTo({
+        url: '/pages/chat/chat?friendId=' + that.data.userInfo.username
+          + '&friendName=' + that.data.userInfo.nickname
+          + '&friendAvatarUrl=' + that.data.userInfo.headshot,
+      })
+    } else {
+
+      //获取朋友用户的identifier和headshot和nickname
+      //在this.data.user里
+
+      app.initUserSig(function cbOk() {
+        // 检查是否登录返回 true 和 false,不登录则重新登录
+        if (im.checkLogin()) {
+          //
+          wx.navigateTo({
+            url: '/pages/chat/chat?friendId=' + that.data.userInfo.username
+              + '&friendName=' + that.data.userInfo.nickname
+              + '&friendAvatarUrl=' + that.data.userInfo.headshot,
+          })
+
+        } else { //做的事情放到回调函数里
+          imhandler.login(that, app, function () {
+            wx.navigateTo({
+              url: '/pages/chat/chat?friendId=' + that.data.userInfo.username
+                + '&friendName=' + that.data.userInfo.nickname
+                + '&friendAvatarUrl=' + that.data.userInfo.headshot,
+            })
+          });
+        }
+        wx.hideLoading()
+      });
+
+    }
+
   },
   onChange(event) {
     this.setData({
